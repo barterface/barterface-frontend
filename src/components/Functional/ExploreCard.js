1c0371295bcb995bcc1a5b04608458bb7142a92a
kishanhitk <kishansharma1231@gmail.com>
2021-08-12 17:09:09 +0530

    feat: Add explore card from ChakraUI

--- src/components/Functional/ExploreCard.js
+++ src/components/Functional/ExploreCard.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useCallback} from "react";
+import React, { useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
-import { Button } from "../UI/Button";
+// import { Button } from "../UI/Button";
 import classes from "./ExploreCard.module.css";
 import RequestModal from "../Modal/RequestModal";
 import { AiOutlineArrowRight, AiFillStar } from "react-icons/ai";
@@ -9,10 +9,8 @@ import { AiOutlineArrowRight, AiFillStar } from "react-icons/ai";
 import * as ratingActions from "../../store/action/ratingAction";
 import awsmobile from "../../aws-exports";
 import { useState } from "react";
-
+import { Box, VStack, Text, HStack, Spacer, Button } from "@chakra-ui/react";
 const ExploreCard = (props) => {
-
-
   const dispatch = useDispatch();
   const [requestModal, setRequestModal] = useState(false);
 
@@ -20,13 +18,12 @@ const ExploreCard = (props) => {
   const ratingData = useSelector((state) => state.rating.availableRating);
   //console.log(ratingData)
 
-  let noOfStars = 0 ;
-  ratingData.forEach(element => {
-    noOfStars = noOfStars + element.noOfStars
+  let noOfStars = 0;
+  ratingData.forEach((element) => {
+    noOfStars = noOfStars + element.noOfStars;
   });
 
   // //const req = useSelector(state => state.request.availableRequest)
-
 
   // const requesterAccId = "12345";
   // const requesterAccPass = "ankur";
@@ -46,19 +43,17 @@ const ExploreCard = (props) => {
   //   );
   // };
 
-  const loadRating = useCallback ( async () => {
+  const loadRating = useCallback(async () => {
     try {
       dispatch(ratingActions.fetchRating(uploaderId));
     } catch (err) {
       console.log(err);
     }
-  },[dispatch])
+  }, [dispatch]);
 
   useEffect(() => {
     loadRating();
   }, [loadRating]);
-
-
 
   return (
     <>
@@ -69,28 +64,79 @@ const ExploreCard = (props) => {
           uploadId={uploadId}
         />
       ) : null}
-      <div className={classes.Card}>
-        <div className={classes.Time}>
-          <h4>{startTime}</h4>
-          <AiOutlineArrowRight />
-          <h4>{endTime}</h4>
-        </div>
-        <div className={classes.CardInfo}>
-          <p>{noOfStars}</p>
-          <p>{name}</p>
-          <p>
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-          </p>
-        </div>
-        <Button buttonColor="red" onClick={()=>setRequestModal(!requestModal)}>
-          Send Request
-        </Button>
-      </div>
+      <Box mr={8} my={5} shadow="lg" rounded="md" p={5}>
+        <VStack alignItems="flex-start" height="100%">
+          <HStack spacing={6} justifyItems="space-between" alignSelf="center">
+            <VStack>
+              <Text fontWeight="bold">
+                {new Date(startTime).toString().slice(4, 10)}
+              </Text>
+              <Text fontWeight="bold">
+                {new Date(startTime).toString().slice(4, 10)}
+              </Text>
+            </VStack>
+            <AiOutlineArrowRight color="grey" />
+            <VStack>
+              <Text fontWeight="bold">
+                {new Date(endTime).toString().slice(4, 10)}
+              </Text>
+              <Text fontWeight="bold">
+                {new Date(endTime).toString().slice(4, 10)}
+              </Text>
+            </VStack>
+          </HStack>
+          <Spacer />
+          <Text fontWeight="bold" pt={2}>
+            7hrs
+          </Text>
+          <Text>{name}</Text>
+          <Text>{`${noOfStars} Ratings`}</Text>
+          <Button
+            onClick={() => setRequestModal(!requestModal)}
+            colorScheme="red"
+            width="100%"
+            alignSelf="center"
+          >
+            Send Request
+          </Button>
+        </VStack>
+      </Box>
     </>
+    // ?? (
+    //   <>
+    //     {requestModal ? (
+    //       <RequestModal
+    //         requestModal={requestModal}
+    //         setRequestModal={setRequestModal}
+    //         uploadId={uploadId}
+    //       />
+    //     ) : null}
+    //     <div className={classes.Card}>
+    //       <div className={classes.Time}>
+    //         <h4>{startTime}</h4>
+    //         <AiOutlineArrowRight />
+    //         <h4>{endTime}</h4>
+    //       </div>
+    //       <div className={classes.CardInfo}>
+    //         <p>{noOfStars}</p>
+    //         <p>{name}</p>
+    //         <p>
+    //           <AiFillStar />
+    //           <AiFillStar />
+    //           <AiFillStar />
+    //           <AiFillStar />
+    //           <AiFillStar />
+    //         </p>
+    //       </div>
+    //       <Button
+    //         buttonColor="red"
+    //         onClick={() => setRequestModal(!requestModal)}
+    //       >
+    //         Send Request
+    //       </Button>
+    //     </div>
+    //   </>
+    // )
   );
 };
 
