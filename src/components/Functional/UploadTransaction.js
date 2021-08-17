6e5cd1ea24c72ae6665e0bb67794ab601a515105
kishanhitk <kishansharma1231@gmail.com>
2021-08-17 17:05:27 +0530

    feat: Add chakraUI to req transactions

--- src/components/Functional/UploadTransaction.js
+++ src/components/Functional/UploadTransaction.js
@@ -1,8 +1,7 @@
-import { Box, Divider, Flex, HStack, Text, VStack } from "@chakra-ui/layout";
+import { Flex, HStack, Text, VStack } from "@chakra-ui/layout";
 import React, { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as uploadActions from "../../store/action/uploadAction";
-import classes from "./UploadTransaction.module.css";
 
 const UploadTransaction = () => {
   const userId = useSelector((state) => state.auth.userId);
@@ -34,45 +33,48 @@ const UploadTransaction = () => {
     >
       {uploadedData.length > 0
         ? uploadedData.map((data) => (
-            <Box shadow="lg" p="10" rounded="xl" mt="10">
-              <HStack
-                justifyContent="center"
-                justifyItems="center"
-                alignItems="center"
-              >
-                <VStack>
-                  <Text fontSize="lg">
-                    {new Date(data.startTime).toString().substr(0, 10)}
-                  </Text>
-                  <Text fontSize="2xl" fontWeight="bold">
-                    {new Date(data.startTime).toString().substr(16, 5)}
-                  </Text>
-                  <Text fontWeight="bold" color="gray">
-                    {data.accName}
-                  </Text>
-                </VStack>
-                <VStack style={{ flex: "2" }}>
-                  <div
-                    style={{
-                      height: "1px",
-                      backgroundColor: "gray",
-                      width: "100%",
-                    }}
-                  ></div>
-                </VStack>
-                <VStack style={{ flex: "1" }}>
-                  <Text fontSize="lg">
-                    {new Date(data.endTime).toString().substr(0, 10)}
-                  </Text>
-                  <Text fontSize="2xl" fontWeight="bold">
-                    {new Date(data.endTime).toString().substr(16, 5)}
-                  </Text>
-                  <Text color="green.400">Uploaded</Text>
-                </VStack>
+            <HStack
+              cursor="pointer"
+              shadow="lg"
+              p="10"
+              rounded="xl"
+              mt="10"
+              justifyContent="center"
+              justifyItems="center"
+              alignItems="center"
+            >
+              <VStack>
+                <Text fontSize="lg">
+                  {new Date(data.startTime).toString().substr(0, 10)}
+                </Text>
+                <Text fontSize="2xl" fontWeight="bold">
+                  {new Date(data.startTime).toString().substr(16, 5)}
+                </Text>
+                <Text fontWeight="bold" color="gray">
+                  {data.accName}
+                </Text>
+              </VStack>
+              <VStack style={{ flex: "2" }}>
+                <div
+                  style={{
+                    height: "1px",
+                    backgroundColor: "gray",
+                    width: "100%",
+                  }}
+                ></div>
+              </VStack>
+              <VStack style={{ flex: "1" }}>
+                <Text fontSize="lg">
+                  {new Date(data.endTime).toString().substr(0, 10)}
+                </Text>
+                <Text fontSize="2xl" fontWeight="bold">
+                  {new Date(data.endTime).toString().substr(16, 5)}
+                </Text>
+                <Text color="green.400">Uploaded</Text>
+              </VStack>
 
-                {/* <div className={classes.CardContainer}></div> */}
-              </HStack>
-            </Box>
+              {/* <div className={classes.CardContainer}></div> */}
+            </HStack>
           ))
         : null}
     </Flex>
